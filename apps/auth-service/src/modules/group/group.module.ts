9f8bb02fff46cb72960e785cb807c84557903204
hsWorld <105342360+hsworld-top@users.noreply.github.com>
2025-06-19 18:04:32 +0800

    修复依赖关系错误

--- apps/auth-service/src/modules/group/group.module.ts
+++ apps/auth-service/src/modules/group/group.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Group } from '../user/entities/group.entity';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
+import { CasbinModule } from '../casbin/casbin.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Group])],
+  imports: [TypeOrmModule.forFeature([Group]), CasbinModule],
   controllers: [GroupController],
   providers: [GroupService],
   exports: [TypeOrmModule, GroupService],
